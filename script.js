54b3ea814469fa8750a7ba9fa7c1ccbc8b4ae780
LQuesadaM <luisquesada163@gmail.com>
2024-03-12 21:34:24 -0300

    🔨 logic for converting to binary

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+
+    input = quotient;
+  }
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
   
+  result.innerText = remainders.reverse().join("");
+  console.log("Remainders reverse: ", remainders);
+
 };
 
 const checkUserInput = () => {
