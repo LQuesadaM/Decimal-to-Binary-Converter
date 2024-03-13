a3ff8e8d466f8d478f113fe4db1930f71b3f7905
LQuesadaM <luisquesada163@gmail.com>
2024-03-13 07:00:51 -0300

    ⚡️ impruve performance

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
