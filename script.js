7dd141f784bf44e1b1d92046424166fb1ae26366
LQuesadaM <luisquesada163@gmail.com>
2024-03-15 09:01:24 -0300

    💄 add new property to animationData object

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ const animationData = [
     inputVal: 5,
     marginTop: 300,
     addElDelay: 1000,
+    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
   },
   {
     inputVal: 2,
     marginTop: -200,
     addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
   },
   {
     inputVal: 1,
     marginTop: -200,
     addElDelay: 2000,
+    msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
   },
 ];
 
